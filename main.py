03bd46fe3629a54d82c9bd5ae96afede351d6b3c
Rana-Kk <ayseranakarakuyu@gmail.com>
2026-08-28 14:29:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
+
+
+if __name__ == "__main__":
+    print("Simple Calculator")
+
+    a = float(input("First number: "))
+    b = float(input("Second number: "))
+
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiply(a, b))
+    print("Division:", divide(a, b))
